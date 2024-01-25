812fc8f15407b5ad89e5d12c6023b83cadf56531
Coeurious_Back_03 <dev_back_3@coeurious.com>
2024-01-25 18:53:11 +0100

    DEV-9 | DEV-39 : TUs

--- src/main/java/com/coeurious/sublimation/service/LoginService.java
+++ src/main/java/com/coeurious/sublimation/service/LoginService.java
@@ -1,11 +1,10 @@
 package com.coeurious.sublimation.service;
 
+import com.coeurious.sublimation.entities.Users;
+import com.coeurious.sublimation.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import com.coeurious.sublimation.entities.Users;
-import com.coeurious.sublimation.repository.UsersRepository;
 
 @Service
 public class LoginService {
@@ -18,16 +17,14 @@ public class LoginService {
 		this.passwordEncoder = passwordEncoder;
 		this.usersRepository = usersRepository;
 	}
-	
+
 	public Users register(Users user) {
-		Users savedUser = null;
 		String hashPassword = passwordEncoder.encode(user.getPassword());
 		
 		user.setPassword(hashPassword);
 		
-		savedUser = usersRepository.save(user);
+		return usersRepository.save(user);
 		
-		return savedUser;
 	}
 
 }
